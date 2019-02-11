20b73e0e4c10d663a1366b43781842a736a5b4fd
Jan König <jankoenig120190@googlemail.com>
2019-02-11 15:12:45 +0100

    🎨 Use SpeechBuilder

--- src/app.js
+++ src/app.js
@@ -29,23 +29,33 @@ const GoogleHandler = require('./google/handler.js');
 
 app.setHandler({
     NEW_USER() {
-        return this.ask('Would you like to begin listening from episode one or rather choose from a list?');
+        this.$speech.addText('Welcome to the Podcast Player!')
+            .addText('Would you like to begin listening from episode one or rather choose from a list?')
+        
+        return this.ask(this.$speech);
     },
 
     LAUNCH() {
-        return this.ask('Would you like to resume where you left off or listen to the latest episode?');
+        this.$speech.addText('Would you like to resume where you left off or listen to the latest episode?')
+        
+        this.ask(this.$speech);
     },
 
     FirstEpisodeIntent() {
         let episode = Player.getFirstEpisode();
         let currentIndex = Player.getEpisodeIndex(episode);
-        this.$user.$data.currentIndex = currentIndex
+        this.$user.$data.currentIndex = currentIndex;
+        this.$speech.addText('Here is the first episode.');
+
         if (this.isAlexaSkill()) {
-            this.$alexaSkill.$audioPlayer.setOffsetInMilliseconds(0).play(episode.url, `${currentIndex}`);
+            this.$alexaSkill.$audioPlayer
+                .setOffsetInMilliseconds(0)
+                .play(episode.url, `${currentIndex}`)
+                .tell(this.$speech);
         } else if (this.isGoogleAction()) {
             this.$googleAction.$audioPlayer.play(episode.url, episode.title);
             this.$googleAction.showSuggestionChips(['pause', 'start over']);
-            this.ask('Enjoy');
+            this.ask(this.$speech);
         }
     },
 
@@ -53,12 +63,17 @@ app.setHandler({
         let episode = Player.getLatestEpisode();
         let currentIndex = Player.getEpisodeIndex(episode);
         this.$user.$data.currentIndex = currentIndex;
+        this.$speech.addText('Here is the latest episode.'); 
+
         if (this.isAlexaSkill()) {
-            this.$alexaSkill.$audioPlayer.setOffsetInMilliseconds(0).play(episode.url, `${currentIndex}`);
+            this.$alexaSkill.$audioPlayer
+                .setOffsetInMilliseconds(0)
+                .play(episode.url, `${currentIndex}`)
+                .tell(this.$speech);
         } else if (this.isGoogleAction()) {
             this.$googleAction.$audioPlayer.play(episode.url, episode.title);
             this.$googleAction.showSuggestionChips(['pause', 'start over']);
-            this.ask('Enjoy');
+            this.ask(this.$speech);
         }
     },
 
@@ -69,7 +84,9 @@ app.setHandler({
         this.$speech.addText('Here\'s a list of episodes: ');
         for (let i = 0; i < indices.length; i++) {
             let episode = Player.getEpisode(indices[i]);
-            this.$speech.addSayAsOrdinal(`${i + 1}`).addText(episode.title).addBreak("100ms");
+            this.$speech.addSayAsOrdinal(`${i + 1}`)
+                .addText(episode.title)
+                .addBreak("100ms");
         }
         this.$speech.addText('Which one would you like to listen to?');
         this.ask(this.$speech);
@@ -81,27 +98,35 @@ app.setHandler({
         let episodeIndex = episodeIndices[parseInt(ordinal.key) - 1];
         this.$user.$data.currentIndex = episodeIndex;
         let episode = Player.getEpisode(episodeIndex);
+        this.$speech.addText('Enjoy'); 
     
         if (this.isAlexaSkill()) {
-            this.$alexaSkill.$audioPlayer.setOffsetInMilliseconds(0).play(episode.url, `${episodeIndex}`);
+            this.$alexaSkill.$audioPlayer
+                .setOffsetInMilliseconds(0)
+                .play(episode.url, `${episodeIndex}`)
+                .tell(this.$speech);
         } else if (this.isGoogleAction()) {
             this.$googleAction.$audioPlayer.play(episode.url, episode.title);
             this.$googleAction.showSuggestionChips(['pause', 'start over']);
-            this.ask('Enjoy');
+            this.ask(this.$speech);
         }
     },
 
     ResumeIntent() {
         let currentIndex = this.$user.$data.currentIndex;
         let episode = Player.getEpisode(currentIndex);
-    
+        this.$speech.addText('Resuming your episode.')
+
         if (this.isAlexaSkill()) {
             let offset = this.$user.$data.offset;
-            this.$alexaSkill.$audioPlayer.setOffsetInMilliseconds(offset).play(episode.url, `${currentIndex}`);
+            this.$alexaSkill.$audioPlayer
+                .setOffsetInMilliseconds(offset)
+                .play(episode.url, `${currentIndex}`)
+                .tell(this.$speech);
         } else if (this.isGoogleAction()) {
             this.$googleAction.$audioPlayer.play(episode.url, episode.title);
             this.$googleAction.showSuggestionChips(['pause', 'start over']);
-            this.ask('Enjoy');
+            this.ask(this.$speech);
         }
     },
 
@@ -140,7 +165,10 @@ app.setHandler({
     },
 
     HelpIntent() {
-        this.ask('You can either listen to episode one or the latest episode or choose from a random list of episodes. Which one would you like to do?');
+        this.$speech.addText('You can either listen to episode one or the latest episode or choose from a random list of episodes.')
+            .addText('Which one would you like to do?')
+
+        this.ask(this.$speech);
     }
 });
 
